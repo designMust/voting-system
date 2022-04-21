86d26fab5e72b9a31b466f9c3ed471c9825ca349
designMust <103852376+designMust@users.noreply.github.com>
2022-04-21 19:20:03 +0200

    Update app.js

--- app.js
+++ app.js
@@ -356,12 +356,13 @@ function userSignedIn() {
         currentVotedList.style.color = "white";
         currentVotedList.style.cursor = "default";
         currentVotedList.textContent = "Votaste";*/
-        console.log("Este usuario votó: " + lista);                   
+        console.log("Este usuario votó: " + lista)//;                   
       }//);
     } else {
       console.log("Usuario ha votado todavía.")
     }
   })
+}
   
   for (const votewrapper of vote) { //Show real vote button
     votewrapper.style.display = "block";
