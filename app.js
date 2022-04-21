cbcfe07062efee768071cfcdc1985b6618044de7
designMust <103852376+designMust@users.noreply.github.com>
2022-04-21 17:33:44 +0200

    Update app.js

--- app.js
+++ app.js
@@ -79,8 +79,8 @@ onAuthStateChanged(auth, (user) => {
       if (snapshot.exists()) {
         let listasVotadas = Object.keys(snapshot.val());
         listasVotadas.forEach( function(lista) {
-          $("#" + lista).style.display = "none";
-          //lista.style.display = "none";
+          let currentVotedList = document.getElemenyById(lista);
+          currentVotedList.style.display = "none";
           console.log(lista)                    
         }); 
         //console.log(snapshot.val()); 
